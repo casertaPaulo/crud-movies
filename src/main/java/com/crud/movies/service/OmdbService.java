ef7a4329d065a6731c3906d021dfb2c50673f659
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-16 13:50:07 -0300

    adding a exception in searchMovie method

--- src/main/java/com/crud/movies/service/OmdbService.java
+++ src/main/java/com/crud/movies/service/OmdbService.java
@@ -2,7 +2,7 @@ package com.crud.movies.service;
 
 import com.crud.movies.dto.MovieDTO;
 import com.crud.movies.dto.MoviesDTO;
-import org.springframework.http.ResponseEntity;
+import com.crud.movies.exceptions.MovieNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,13 +10,24 @@ import java.util.List;
 
 @Service
 public class OmdbService {
-    private final String URL_SEARCH = "https://omdbapi.com/?apikey=269dedd8&s=";
+    private final String URL_SEARCH = "https://omdbapi.com/?apikey=269dedd8&";
 
     RestTemplate restTemplate = new RestTemplate();
 
-    public List<MovieDTO> searchMovies(String title) {
-        MoviesDTO moviesDTO = restTemplate.getForObject(URL_SEARCH + title.replace(" ", "+"), MoviesDTO.class);
+    public List<MovieDTO> searchMovie(String title) throws Exception {
+        MoviesDTO moviesDTO = restTemplate.getForObject(
+                URL_SEARCH + "s=" + title.replace(" ", "+"), MoviesDTO.class);
+
         assert moviesDTO != null;
+        if (moviesDTO.getMovieDTOS() == null) {
+            throw new MovieNotFoundException();
+        }
+
         return moviesDTO.getMovieDTOS();
+    }
+
+    public MovieDTO getMovie(String title) {
+        return restTemplate.getForObject(
+                URL_SEARCH + "t=" + title.replace(" ", "+"), MovieDTO.class);
     }
 }
