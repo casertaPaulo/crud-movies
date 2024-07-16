f804de602be238d4f3b0dbc641c7ff7bee22ffdd
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-16 13:48:38 -0300

    implementing the cors config

--- src/main/java/com/crud/movies/infra/CorsConfig.java
+++ src/main/java/com/crud/movies/infra/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.crud.movies;
+package com.crud.movies.infra;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
